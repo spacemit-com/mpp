623a278f9a20e93d7faf519d794c907ee59cd61a
fuqiang <qiang.fu@spacemit.com>
2024-09-12 19:32:40 +0800

    fix bug: ffmpeg decode->encode output stream do not have timestamp

--- al/vcodec/v4l2/linlonv5v7/linlonv5v7_enc.c
+++ al/vcodec/v4l2/linlonv5v7/linlonv5v7_enc.c
@@ -791,6 +791,7 @@ S32 al_enc_get_output_stream(ALBaseContext *ctx, MppData *src_data) {
              getUserPtr(buffer, 0), b->bytesused);
     }
     PACKET_SetLength(PACKET_GetPacket(src_data), b->bytesused);
+    PACKET_SetPts(PACKET_GetPacket(src_data), (S64)(b->timestamp.tv_sec * 1000000 + b->timestamp.tv_usec));
     resetVendorFlags(buffer);
     queueBuffer(getOutputPort(context->stCodec), buffer);
 
