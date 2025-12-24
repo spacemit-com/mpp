d31920244e8dc09759c70706e278e40d207ec818
wumingjie <mingjie.wu@spacemit.com>
2025-12-24 09:50:36 +0800

    fix compile error

--- al/vps/k1/v2d/include/asr_v2d_type.h
+++ al/vps/k1/v2d/include/asr_v2d_type.h
@@ -9,7 +9,7 @@
 #include <fcntl.h>
 #include <unistd.h>
 #include <asm-generic/ioctl.h>
-
+#include <stdbool.h>
 #define V2D_LOG_LEVEL_DEBUG
 #ifdef V2D_LOG_LEVEL_DEBUG
 #define V2DLOGD(fmt, ...) printf(fmt, ## __VA_ARGS__)
@@ -31,7 +31,6 @@ typedef unsigned short uint16_t;
 typedef unsigned int uint32_t;
 typedef unsigned long uint64_t;
 typedef uint64_t V2D_HANDLE;
-typedef int bool;
 
 typedef enum asrV2D_INPUT_LAYER_E {
     V2D_INPUT_LAYER0    =0,
